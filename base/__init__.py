6493d5b25ae485b0a66ed40c2eeaca46a589aeda
Amin <ma.maleki131@gmail.com>
2022-11-12 11:49:06 +0330

    Getting Started Templates Forms and User Input Database with Flask-SQLAlchemy Package Structure User Authentication

--- base/__init__.py
+++ base/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "da807d45fe597024e301da7b7426c0a3"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
 with app.app_context():
@@ -14,7 +14,7 @@ with app.app_context():
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from base import routes
