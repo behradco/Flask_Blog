a83c5edbe817ed6d8aa542d2d35c3fe44ec43b5c
Amin <ma.maleki131@gmail.com>
2022-11-12 11:08:18 +0330

    Getting Started Templates Forms and User Input Database with Flask-SQLAlchemy Package Structure User Authentication

--- base/models.py
+++ base/models.py
@@ -1,7 +1,13 @@
+import os
 from datetime import datetime
-from base import db
+from base import app, db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -11,7 +17,6 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
